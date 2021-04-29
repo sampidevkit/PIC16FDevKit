4e4e81f176727e275953d0139340d3d206fb2a06
SAMPI Development Kit <sampidevkit@gmail.com>
2021-04-29 21:28:53 +0700

    SAMPI BOOTLOADER SAMM-75/6/7-F|R|T v2.0 PIC32MM0064GPM028 Rel: Apr 29 2021-21:24:25

--- FW/PG_BLD.X/mcc_generated_files/mcc.h
+++ FW/PG_BLD.X/mcc_generated_files/mcc.h
@@ -51,11 +51,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "memory/flash.h"
-#include "coretimer.h"
+#include "watchdog.h"
 #include "interrupt_manager.h"
 #include "exceptions.h"
-#include "watchdog.h"
+#include "memory/flash.h"
+#include "coretimer.h"
 #include "usb/usb.h"
 
 #warning "This file will be removed in future MCC releases. Use system.h instead."
--- FW/PG_BLD.X/mcc_generated_files/system.c
+++ FW/PG_BLD.X/mcc_generated_files/system.c
@@ -83,10 +83,10 @@
 #include "pin_manager.h"
 #include "clock.h"
 #include "system.h"
-#include "memory/flash.h"
-#include "coretimer.h"
 #include "interrupt_manager.h"
 #include "exceptions.h"
+#include "memory/flash.h"
+#include "coretimer.h"
 #include "usb/usb.h"
 
 void SYSTEM_Initialize(void)
