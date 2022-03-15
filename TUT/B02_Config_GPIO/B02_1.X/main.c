c6d5b5f054d6e5ba7c0caa46f903f5d5fe3300b1
SAMPI Dev Kit <sampidevkit@gmail.com>
2022-03-15 21:31:36 +0700

    Present 1: Cfg - GPIO update 3

--- TUT/B02_Config_GPIO/B02_1.X/main.c
+++ TUT/B02_Config_GPIO/B02_1.X/main.c
@@ -1,19 +1,10 @@
-/*
- * File:   main.c
- * Author: sampi
- *
- * Created on January 21, 2022, 10:28 PM
- */
-
-// PIC16F18877 Configuration Bit Settings
-// 'C' source line config statements
+// <editor-fold defaultstate="collapsed" desc="PIC16F18877 Configuration Bit Settings">
 // CONFIG1
 #pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (Oscillator not enabled)
 #pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
 #pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
 #pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
 #pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (FSCM timer enabled)
-
 // CONFIG2
 #pragma config MCLRE = ON       // Master Clear Enable bit (MCLR pin is Master Clear function)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
@@ -23,25 +14,19 @@
 #pragma config ZCD = OFF        // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
 #pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
 #pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
-
 // CONFIG3
 #pragma config WDTCPS = WDTCPS_31// WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
 #pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
 #pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
 #pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
-
 // CONFIG4
 #pragma config WRT = OFF        // UserNVM self-write protection bits (Write protection off)
 #pragma config SCANE = available// Scanner Enable bit (Scanner module is available for use)
 #pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
-
 // CONFIG5
 #pragma config CP = OFF         // UserNVM Program memory code protection bit (Program Memory code protection disabled)
 #pragma config CPD = OFF        // DataNVM code protection bit (Data EEPROM code protection disabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
+// </editor-fold>
 #include <xc.h>
 
 #define _XTAL_FREQ 32000000 // System clock
@@ -49,22 +34,20 @@
 void main(void)
 {
     // Oscillator configure, data sheet pg. 121-126
-    OSCCON1=0x60; // NOSC HFINTOSC; NDIV 1; 
-    OSCCON3=0x00; // CSWHOLD may proceed; SOSCPWR Low power; 
-    OSCEN=0x00; // MFOEN disabled; LFOEN disabled; ADOEN disabled; SOSCEN disabled; EXTOEN disabled; HFOEN disabled; 
-    OSCFRQ=0x06; // HFFRQ 32_MHz;
-    OSCTUNE=0x00; // HFTUN 0; 
+    OSCCON1=0b01100000; // NOSC HFINTOSC; NDIV 1; 
+    OSCFRQ=0b00000110; // HFFRQ 32_MHz;
 
     // GPIO configure
-    LATAbits.LATA1=0; // State of A1 is low
-    TRISAbits.TRISA1=0; // A1 is an output
     ANSELAbits.ANSA1=0; // A1 signal is digital
+    TRISAbits.TRISA1=0; // A1 is an output
+    LATAbits.LATA1=0; // State of A1 is low
 
+    ANSELDbits.ANSD3=0; // D3 signal is digital
+    TRISDbits.TRISD3=1; // D3 is an output
+    WPUDbits.WPUD3=1; // D3 is pulled up
+    
     while(1)
     {
-        LATAbits.LATA1^=1; // Toggle pin A1
-        __delay_ms(250);
+        LATAbits.LATA1=~PORTDbits.RD3;
     }
-
-    return;
 }
